81faa24d73cf170ea2aca6c661e64faa9b4442ae
whuizenga <will.huizenga@gmail.com>
2017-09-05 11:13:21 -0400

    seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require_relative './song_data.rb'
+require_relative './artist_data.rb'
+
+Song.destroy_all
+Artist.destroy_all
+
+song_data = get_song_data
+artist_data = get_artist_data
+
+song_data.each_pair do |artist_name, songs|
+  info = artist_data[artist_name]
+  current_artist = Artist.create!({
+    name:         info[:name],
+    photo_url:    info[:photo_url],
+    nationality:  info[:nationality]
+  })
+
+  songs.each do |song|
+    Song.create!({
+      title:        song[:title],
+      album:        song[:album],
+      preview_url:  song[:preview_link],
+      artist:       current_artist
+    })
+  end
+end
